6f3a77ab2e45fdd7f000c15874770132a3ad5d3b
Cartr1X <90981958+Cartr1X@users.noreply.github.com>
2026-02-12 17:35:51 +0300

    Add files via upload

--- LABA#2/LABA2.py
+++ LABA#2/LABA2.py
@@ -1,3 +1,11 @@
-def sum_range(star, end):
-
-# sum_range(start, end) без циклов через рекурс
+x = int(input("Start: "))
+y = int(input("End: "))
+def sum_range(start, end):
+    if start == end:
+        return start
+    elif start < end:
+        return start + sum_range(start + 1, end) # если начально число меньше конечного
+    else:
+        return start + sum_range(start - 1, end) # в обратную сторону
+    
+print(sum_range(x, y))
